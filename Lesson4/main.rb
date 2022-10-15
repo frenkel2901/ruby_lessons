2ef91d9a34de12fab5d2e356305dfccbbdfe6aef
frenkel2901 <a.frenkel2901@gmail.com>
2022-10-15 13:15:39 +0300

    redo

--- Lesson4/main.rb
+++ Lesson4/main.rb
@@ -27,3 +27,69 @@ def create_station
   puts name_station.depo
 
 end
+
+def create_train
+
+  puts "1. Грузовой поезд"
+  puts "2. Пассажирский поезд"
+  i_type = gets.chomp.to_i
+  print "Ведите номер поезда: "
+  num_train = gets.chomp
+  num_train = CargoTrain.new(num_train) if i_type == 1
+  num_train = PassangerTrain.new(num_train) if i_type == 2
+
+  puts num_train
+end
+
+def route_manager
+  puts "add - Создать маршрут"
+  puts "new - Добавить станцию в маршрут"
+  puts "del - Удалить станцию из маршрута"
+  i_route = gets.chomp.to_s
+
+  if i_route == 'add'
+
+  print "Ведите название маршрута: "
+  name_route = gets.chomp
+  
+  print "Создайте первую станцию: "
+  first_station = gets.chomp
+  first_station = Station.new(first_station)
+
+  print "Создайте последнюю станцию: "
+  last_station = gets.chomp
+  last_station = Station.new(last_station)
+
+  name_route = Route.new(first_station, last_station)
+
+  puts name_route
+
+  end
+
+  if i_route == 'new'
+    puts "Добавить существующую станцию в маршрут"
+
+    print "Ведите название станции: "
+    new_station = gets.chomp
+
+    print "Ведите название маршрута: "
+    route_choise = gets.chomp
+    route_choise.new_station(new_station)
+  end
+
+  if i_route == 'del'
+    puts "Удалить станцию из маршрута"
+
+    print "Ведите название маршрута: "
+    route_choise = gets.chomp
+
+    print "Ведите название станции: "
+    del_station = gets.chomp
+
+    route_choise.delete_station(del_station)
+  end
+end
+
+def all_info
+  puts $stations_info
+end
